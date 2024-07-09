4477e5c7bc12e1e8ee2bb7b1b7d0ddc65e01f044
송선권 <songsunkook@gmail.com>
2024-07-09 12:56:38 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,37 @@
+/*
+    BAEKJOON 1629번 곱셈
+    https://www.acmicpc.net/problem/1629
+
+    추가 학습 내용
+    - 
+
+    복습 필요: O
+    풀이 참고: O
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+        long C = Integer.parseInt(st.nextToken());
+        System.out.print(func(A, B, C));
+    }
+
+    static long func(long a, long b, long c) {
+        if (b <= 1) {
+            return a % c;
+        }
+        long num = func(a, b / 2, c);
+        num = num * num % c;
+        if (b % 2 == 0) {
+            return num;
+        }
+        return num * a % c;
+    }
+}
