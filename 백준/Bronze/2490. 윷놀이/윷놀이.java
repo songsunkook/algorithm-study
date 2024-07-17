60775b133038478bfe6653b4743f05fe3d8f5e37
송선권 <songsunkook@gmail.com>
2024-07-17 14:17:32 +0900

    [Bronze III] Title: 윷놀이, Time: 124 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -0,0 +1,44 @@
+/*
+    BAEKJOON 2490번 윷놀이
+    https://www.acmicpc.net/problem/2490
+
+    추가 학습 내용
+    - 
+
+    복습 필요: x
+    풀이 참고: x
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            for (int j = 0; j < 4; j++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            switch (sum) {
+                case 0:
+                    System.out.println("D");
+                    break;
+                case 1:
+                    System.out.println("C");
+                    break;
+                case 2:
+                    System.out.println("B");
+                    break;
+                case 3:
+                    System.out.println("A");
+                    break;
+                case 4:
+                    System.out.println("E");
+                    break;
+            }
+        }
+    }
+}
