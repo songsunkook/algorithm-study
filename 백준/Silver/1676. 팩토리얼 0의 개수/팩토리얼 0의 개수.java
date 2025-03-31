4e34101f530b143be4aa7feffbe3f06a627cbcf8
송선권 <songsunkook@gmail.com>
2025-03-31 11:22:00 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -2,9 +2,7 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int count2 = 0;
     static int count5 = 0;
-    static Integer[] dp2 = new Integer[501];
     static Integer[] dp5 = new Integer[501];
 
     public static void main(String[] args) throws IOException {
@@ -13,19 +11,13 @@ class Main {
 
         int n = Integer.parseInt(br.readLine());
         fac(n);
-        bw.write(Math.min(count2, count5) + "");
+        bw.write(count5 + "");
         bw.close();
         br.close();
     }
 
     private static void fac(int n) {
         for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                if (dp2[i / 2] == null)
-                    dp2[i / 2] = 0;
-                count2 += dp2[i / 2] + 1;
-                dp2[i] = dp2[i / 2] + 1;
-            }
             if (i % 5 == 0) {
                 if (dp5[i / 5] == null)
                     dp5[i / 5] = 0;
