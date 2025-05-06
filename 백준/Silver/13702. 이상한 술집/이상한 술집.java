57e8e3a3608ad65638e97ce9be8020deaabc17e8
송선권 <songsunkook@gmail.com>
2025-05-06 13:58:57 +0900

    [Silver II] Title: 이상한 술집, Time: 168 ms, Memory: 17952 KB -BaekjoonHub

---
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, arr[i]);
+        }
+
+        if (max == 0) {
+            bw.write("0");
+            bw.close();
+            br.close();
+            return;
+        }
+
+        long l = 1, r = max;
+        long result = 0;
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            if (countOf(arr, mid) >= k) {
+                result = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        bw.write(result + "");
+
+        bw.close();
+        br.close();
+    }
+
+    static long countOf(int[] arr, long thre) {
+        long result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result += (long) arr[i] / thre;
+        }
+        return result;
+    }
+}
