613c4f15e282802e09e980a26f76e75976880c31
송선권 <songsunkook@gmail.com>
2025-05-11 14:19:06 +0900

    [Silver I] Title: 신입 사원, Time: 804 ms, Memory: 298772 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -9,20 +9,19 @@ class Main {
         int T = Integer.parseInt(br.readLine());
         for (int t = 0; t < T; t++) {
             int n = Integer.parseInt(br.readLine());
-            P[] arr = new P[n];
+            int[] arr = new int[n];
             for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-                arr[i] = new P(a, b);
+                arr[a - 1] = b;
             }
-            Arrays.sort(arr, (a, b) -> Integer.compare(a.a, b.a));
 
             int result = 1;
-            int minB = arr[0].b;
+            int minB = arr[0];
             for (int i = 1; i < n; i++) {
-                if (minB > arr[i].b) {
-                    minB = arr[i].b;
+                if (minB > arr[i]) {
+                    minB = arr[i];
                     result++;
                 }
             }
