3124cc8b083753d0ec824493c3207d83ba6e09d9
송선권 <songsunkook@gmail.com>
2025-05-27 21:58:50 +0900

    [Bronze V] Title: 크냐?, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a == 0 && b == 0)
+                break;
+            bw.write(Integer.compare(a, b) > 0 ? "Yes\n" : "No\n");
+        }
+
+        bw.close();
+        br.close();
+    }
+}
