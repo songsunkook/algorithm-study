cfb5ca29922c45c0916603574b556d9b65f651c8
송선권 <songsunkook@gmail.com>
2025-07-05 22:37:24 +0900

    [Bronze V] Title: 기찍 N, Time: 244 ms, Memory: 21100 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i > 0; i--)
+            bw.write(i + "\n");
+
+        bw.close();
+        br.close();
+    }
+}
