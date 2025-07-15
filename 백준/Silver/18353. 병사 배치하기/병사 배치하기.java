af5bc6d8501e089d27607f535b366fb97c1e6f7f
송선권 <songsunkook@gmail.com>
2025-07-15 09:49:23 +0900

    [Silver II] Title: 병사 배치하기, Time: 144 ms, Memory: 14700 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -7,31 +7,24 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // 오름차순 기준으로 변경
+        int[] arr = new int[n];
         for (int i = n - 1; i >= 0; i--) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        // LIS
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-
-        for (int i = 1; i < n; i++) {
+        int max = 0;
+        int[] len = new int[n];
+        int totalMax = 0;
+        for (int i = 0; i < n; i++) {
+            len[i] = 1;
             for (int j = 0; j < i; j++) {
                 if (arr[j] < arr[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    len[i] = Math.max(len[i], len[j] + 1);
                 }
             }
+            totalMax = Math.max(totalMax, len[i]);
         }
-
-        int lis = 0;
-        for (int i = 0; i < n; i++) {
-            lis = Math.max(lis, dp[i]);
-        }
-
-        bw.write((n - lis) + "\n");
+        bw.write(n - totalMax + "");
         bw.close();
         br.close();
     }
