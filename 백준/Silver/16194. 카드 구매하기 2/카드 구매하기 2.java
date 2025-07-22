41e5c02036789bc1f1fd4c9181e553b974b7c7c3
송선권 <songsunkook@gmail.com>
2025-07-22 09:47:38 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 128 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -2,26 +2,34 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    static int[] arr, dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n + 1];
-        int[] dp = new int[n + 1];
+        arr = new int[n + 1];
+        dp = new int[n + 1];
+        Arrays.fill(dp, 1000000000);
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        dp[0] = 0;
         for (int i = 1; i <= n; i++) {
-            dp[i] = Integer.MAX_VALUE;
-            for (int j = 1; j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[j - 1] + arr[i - (j - 1)]);
-            }
+            dp[i] = Math.min(arr[i], min(i));
         }
         bw.write(dp[n] + "");
+
         bw.close();
         br.close();
+    }
+
+    static int min(int num) {
+        int m = Integer.MAX_VALUE;
+        for (int i = 0; i <= num; i++) {
+            m = Math.min(m, dp[i] + dp[num - i]);
+        }
+        return m;
     }
 }
