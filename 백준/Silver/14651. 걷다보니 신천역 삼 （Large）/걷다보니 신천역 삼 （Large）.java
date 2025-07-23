139d73ed39e2a8d0552668811d4d89f1a55a0d6e
송선권 <songsunkook@gmail.com>
2025-07-23 09:59:39 +0900

    [Silver I] Title: 걷다보니 신천역 삼 (Large), Time: 120 ms, Memory: 17520 KB -BaekjoonHub

--- 백준/Silver/14651. 걷다보니 신천역 삼 （Large）/걷다보니 신천역 삼 （Large）.java
+++ 백준/Silver/14651. 걷다보니 신천역 삼 （Large）/걷다보니 신천역 삼 （Large）.java
@@ -7,20 +7,19 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-
-        long[][] dp = new long[n + 1][3];
-        dp[1][0] = 0;// n = 1 , mod = 0
+        long[][] dp = new long[n + 1][10]; // idx , mod
+        dp[1][0] = 0;
         dp[1][1] = 1;
         dp[1][2] = 1;
-        for (int i = 1; i < n; i++) {
-            for (int mod = 0; mod < 3; mod++) {
-                for (int digit = 0; digit <= 2; digit++) {
-                    int nextMod = (mod + digit) % 3;
-                    dp[i + 1][nextMod] = (dp[i + 1][nextMod] + dp[i][mod]) % 1000000009;
+        for (int i = 2; i <= n; i++) {
+            for (int befD = 0; befD <= 2; befD++) {
+                for (int d = 0; d <= 2; d++) {
+                    int mod = (befD + d) % 3;
+                    dp[i][mod] = dp[i][mod] + dp[i - 1][befD] % 1000000009;
                 }
             }
         }
-        bw.write(dp[n][0] + "");
+        bw.write(dp[n][0] % 1000000009 + "");
         bw.close();
         br.close();
     }
