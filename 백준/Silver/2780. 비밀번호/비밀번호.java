8dd4c27eaf2bbe4876390b2f857d85290245e949
송선권 <songsunkook@gmail.com>
2025-07-30 13:42:32 +0900

    [Silver I] Title: 비밀번호, Time: 160 ms, Memory: 17980 KB -BaekjoonHub

--- 백준/Silver/2780. 비밀번호/비밀번호.java
+++ 백준/Silver/2780. 비밀번호/비밀번호.java
@@ -2,58 +2,62 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    /**
+     * 1 2 3
+     * 4 5 6
+     * 7 8 9
+     * 0
+     * 
+     * for(i = 0 ~ 9)
+     * for(a = adj(i))
+     * dp[cur][i] += dp[cur-1][a]
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        long[][] dp = new long[1001][10];
-        int T = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[1001][10];
         Arrays.fill(dp[1], 1);
-        for (int t = 0; t < T; t++) {
-            int TIME = Integer.parseInt(br.readLine());
-            for (int time = 2; time <= TIME; time++) {
-                if (dp[time][0] != 0)
+        for (int i = 0; i < n; i++) {
+            int count = Integer.parseInt(br.readLine());
+            for (int turn = 2; turn <= count; turn++) {
+                if (dp[turn][0] != 0)
                     continue;
-                for (int num = 0; num < 10; num++) {
-                    for (int side : getSide(num)) {
-                        dp[time][num] += dp[time - 1][side] % 1234567;
+                for (int digit = 0; digit <= 9; digit++) {
+                    for (var adjDigit : adj(digit)) {
+                        dp[turn][digit] += dp[turn - 1][adjDigit] % 1234567;
                     }
                 }
             }
             long sum = 0;
-            for (int i = 0; i < 10; i++)
-                sum += dp[TIME][i] % 1234567;
+            for (int digit = 0; digit < 10; digit++)
+                sum += dp[count][digit] % 1234567;
             bw.write(sum % 1234567 + "\n");
         }
+
         bw.close();
         br.close();
     }
 
     static int[][] dd = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-    static int[][] board = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 0, -1, -1 } };
+    static int[][] pad = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 0, -1, -1 } };
 
-    static List<Integer> getSide(int num) {
-        int r = 0, c = 0;
-        for (int i = 0; i < 10; i++) {
-            r = i / 3;
-            c = i % 3;
-            if (board[r][c] == num)
-                break;
-        }
-        List<Integer> res = new ArrayList<>();
+    static List<Integer> adj(int n) {
+        List<Integer> result = new ArrayList<>();
+        if (n == 0)
+            return List.of(7);
+        if (n == 7)
+            return List.of(4, 8, 0);
+        int r = (n - 1) / 3;
+        int c = (n - 1) % 3;
         for (int i = 0; i < 4; i++) {
             int nr = r + dd[i][0];
             int nc = c + dd[i][1];
-            if (in(nr, nc)) {
-                res.add(board[nr][nc]);
-            }
+            if (nr >= 3 || nc >= 3 || nr < 0 || nc < 0 || pad[nr][nc] == -1)
+                continue;
+            result.add(pad[nr][nc]);
         }
-        return res;
-    }
-
-    static boolean in(int r, int c) {
-        if (r == 3 && c > 0)
-            return false;
-        return r >= 0 && c >= 0 && r <= 3 && c < 3;
+        return result;
     }
 }
