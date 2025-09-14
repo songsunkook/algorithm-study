9ea2cec9e4145a923dba5bda6b60e9a7a598745b
송선권 <songsunkook@gmail.com>
2025-09-14 15:01:22 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -29,25 +29,21 @@ class Main {
         if (n >= 1) {
             dp[0][0] = 0;
             dp[0][1] = arr[0];
-            dp[0][2] = -10000000;
+            dp[0][2] = 0;
         }
         if (n >= 2) {
             dp[1][0] = arr[0];
             dp[1][1] = arr[1];
             dp[1][2] = arr[0] + arr[1];
         }
-        int ans = Math.max(func(n - 1, 1), func(n - 1, 2));
+        for (int i = 2; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = Math.max(dp[i - 1][0], Math.max(dp[i - 2][1], dp[i - 2][2])) + arr[i];
+            dp[i][2] = dp[i - 1][1] + arr[i];
+        }
+        int ans = Math.max(dp[n - 1][1], dp[n - 1][2]);
         bw.write(ans + "");
         bw.close();
         br.close();
-    }
-
-    static int func(int i, int j) {
-        if (dp[i][j] != null)
-            return dp[i][j];
-        dp[i][0] = Math.max(func(i - 1, 1), func(i - 1, 2));
-        dp[i][1] = Math.max(func(i - 1, 0), Math.max(func(i - 2, 1), func(i - 2, 2))) + arr[i];
-        dp[i][2] = func(i - 1, 1) + arr[i];
-        return dp[i][j];
     }
 }
