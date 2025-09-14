ab33e6cfbb5ff0dea6c8dd24520a64de3030e167
송선권 <songsunkook@gmail.com>
2025-09-14 14:26:56 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,41 +1,53 @@
-/*
-    BAEKJOON 2579번 계단 오르기
-    https://www.acmicpc.net/problem/2579
-
-    추가 학습 내용
-    - 
-
-    복습 필요: x
-    풀이 참고: x
-*/
-
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-    static int N;
-    static int[] score;
-    static int[][] d;
+/*
+ * dp[i] = i번째 계단을 밟는 가장 큰 값
+ * dp[i] = Math.max(dp[i-1], dp[i-2]);
+ * 
+ * dp[i][j] = i번째 계단을 j번째로 밟는 가장 큰 값
+ * dp[i][0] = Math.max(dp[i-1][1], dp[i-1][2])
+ * dp[i][1] = Math.max(dp[i-1][0], dp[i-2][1], dp[i-2],[2]) + now
+ * dp[i][2] = dp[i-1][1] + now
+ * 
+ * answer: Math.max(dp[n-1][1], dp[n-1][2])
+ */
+class Main {
+    static int[] arr;
+    static Integer[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        d = new int[N + 1][3];
-        score = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            score[i] = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        dp = new Integer[n][3];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        d[1][1] = score[1];
-        d[1][2] = -100000;
-        if (N >= 2) {
-            d[2][1] = score[2];
-            d[2][2] = score[1] + score[2];
+        if (n >= 1) {
+            dp[0][0] = 0;
+            dp[0][1] = arr[0];
+            dp[0][2] = -10000000;
         }
-        for (int i = 3; i <= N; i++) {
-            d[i][1] = Math.max(d[i - 2][1], d[i - 2][2]) + score[i];
-            d[i][2] = d[i - 1][1] + score[i];
+        if (n >= 2) {
+            dp[1][0] = arr[0];
+            dp[1][1] = arr[1];
+            dp[1][2] = arr[0] + arr[1];
         }
-        System.out.print(Math.max(d[N][1], d[N][2]));
+        int ans = Math.max(func(n - 1, 1), func(n - 1, 2));
+        bw.write(ans + "");
+        bw.close();
+        br.close();
+    }
+
+    static int func(int i, int j) {
+        if (dp[i][j] != null)
+            return dp[i][j];
+        dp[i][0] = Math.max(func(i - 1, 1), func(i - 1, 2));
+        dp[i][1] = Math.max(func(i - 1, 0), Math.max(func(i - 2, 1), func(i - 2, 2))) + arr[i];
+        dp[i][2] = func(i - 1, 1) + arr[i];
+        return dp[i][j];
     }
 }
