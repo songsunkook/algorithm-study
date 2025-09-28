794848f10fcaae32c20075c56c09919daaa08c08
송선권 <songsunkook@gmail.com>
2025-09-28 20:02:41 +0900

    [Silver I] Title: 카드 구매하기, Time: 128 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -2,28 +2,23 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int n;
-    static int[] dp;
-    static int[] arr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n + 1];
-        dp = new int[n + 1];
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        dp[1] = arr[1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                dp[i] = Math.max(dp[i], arr[j] + dp[i - j]);
+                dp[i] = Math.max(dp[i], dp[i - j] + arr[j]);
             }
         }
-
         bw.write(dp[n] + "");
         bw.close();
         br.close();
