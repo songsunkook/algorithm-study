025284c4c64471e0242d4bd94b2b095e0125a09a
송선권 <songsunkook@gmail.com>
2025-10-29 11:23:45 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 620 ms, Memory: 54004 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,45 +1,34 @@
-/*
-    BAEKJOON 11659번 구간 합 구하기 4
-    https://www.acmicpc.net/problem/11659
-
-    추가 학습 내용
-    - 
-
-    복습 필요: o
-    풀이 참고: o
-*/
-
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-    static int N, M;
-    static int[] num;
-    static int[] d;
-
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        num = new int[N + 1];
-        d = new int[N + 1];
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        int[] sum = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (i != 0)
+                sum[i] = sum[i - 1] + arr[i];
+            else
+                sum[i] = arr[i];
         }
-        d[1] = num[1];
-        for (int i = 2; i <= N; i++) {
-            d[i] = d[i - 1] + num[i];
-        }
-        for (int t = 0; t < M; t++) {
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            bw.write((d[e] - d[s - 1]) + "\n");
+            int s = Integer.parseInt(st.nextToken()) - 1;
+            int e = Integer.parseInt(st.nextToken()) - 1;
+            bw.write(sum[e] - (s > 0 ? sum[s - 1] : 0) + "\n");
         }
+
         bw.close();
+        br.close();
     }
 }
