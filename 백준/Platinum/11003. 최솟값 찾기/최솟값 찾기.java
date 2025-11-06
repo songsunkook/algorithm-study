fea58320a25e15b619399311878bab97d4cc83a1
송선권 <songsunkook@gmail.com>
2025-11-06 10:37:03 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2108 ms, Memory: 612248 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,55 +1,42 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * class N {idx, num} 정의
- * n, L 입력
- * for(i=0 ~ n){
- * next 입력
- * while(!q.isEmpty() && q.peekLast() >= next)
- * q.pollLast()
- * q.offerLast(new N(i, next))
- * 
- * // 윈도우 사이즈 체크
- * if (i - q.peekFirst() > L)
- * q.pollFirst();
- * q.peekFirst().num 출력
- * }
- */
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
-        Deque<N> q = new LinkedList<>();
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int next = Integer.parseInt(st.nextToken());
-            while (!q.isEmpty() && q.peekLast().num >= next) {
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Deque<Node> q = new LinkedList<>();
+        for (int r = 0; r < N; r++) {
+            while (!q.isEmpty() && q.peekLast().n >= A[r]) {
                 q.pollLast();
             }
-            q.offerLast(new N(i, next));
-
-            if (i - q.peekFirst().idx >= L) {
+            q.offerLast(new Node(A[r], r));
+            while (q.peekFirst().i < r - L + 1) {
                 q.pollFirst();
             }
-            bw.write(q.peekFirst().num + " ");
+            bw.write(q.peekFirst().n + " ");
         }
 
         bw.close();
         br.close();
     }
 
-    static class N {
-        int idx;
-        int num;
+    static class Node {
+        int n;
+        int i;
 
-        N(int i, int n) {
-            idx = i;
-            num = n;
+        Node(int nn, int ii) {
+            n = nn;
+            i = ii;
         }
     }
 }
