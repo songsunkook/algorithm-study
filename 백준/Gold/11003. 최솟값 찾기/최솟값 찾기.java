afde74631df359031cf97c0c7263cd5be6c37c3a
송선권 <songsunkook@gmail.com>
2026-03-09 16:47:38 +0900

    [Gold I] Title: 최솟값 찾기, Time: 2176 ms, Memory: 661244 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.java
@@ -2,11 +2,18 @@ import java.io.*;
 import java.util.*;
 
 /**
- * D(i) = A(i-L+1) ~ A(i)
- * D(i) = 길이가 L인 슬라이딩 윈도우 내부의 최소값
- * i가 음수인 경우는 무시한다.
- * 범위 5백만은 최소값을 O(n)에 꺼낼 수 있어야 한다.
- * 덱에서 이전 인덱스일수록 이후에 비해 작은 값을 가지도록 강제하면 O(n)에 정렬을 구현할 수 있다.
+ * 슬라이딩 윈도우
+ * 숫자 하나가 윈도우에 들어오고 나갈 때마다 최소값을 구해야 한다.
+ * 우선순위 큐 사용 시 O(log n)을 숫자 삽입/삭제마다 해야 한다.
+ * 숫자는 총 500만 개. 1억을 초과한다.
+ * 
+ * O(n) 이내에 완료해야 한다.
+ * 완전한 정렬을 할 필요는 없다. 필요한 값은 결국 특정 구간에서 가장 작은 값 뿐이다.
+ * (idx, value)로 값을 관리한다.
+ * 큐를 사용하며, cur를 1씩 올리며 front에서 cur > idx인 경우 제거한다.
+ * back에는 수가 하나씩 들어올텐데, 이전 값보다 작은 value가 들어오면 이전 값을 지워버린다.
+ * 최소값만 있으면 되니 불필요한 값이기 때문이다.
+ * 앞의 값이 더 크면 제거함으로써 front node가 가장 작음을 보장할 수 있다.
  */
 class Main {
     public static void main(String[] args) throws IOException {
@@ -15,32 +22,30 @@ class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
-        int[] a = new int[n];
+        int size = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int[] ans = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int l = -s + 1, r = 0;
-        int min = a[0];
-        int[] d = new int[n];
+
+        int l = -size + 1, r = 0;
         Deque<N> dq = new LinkedList<>();
-        dq.addFirst(new N(0, a[0]));
-        for (int i = 0; i < n; i++) {
+        while (r < n) {
+            while (!dq.isEmpty() && dq.peekLast().val >= arr[r]) {
+                dq.removeLast();
+            }
+            dq.addLast(new N(r, arr[r]));
             while (!dq.isEmpty() && dq.peekFirst().idx < l) {
                 dq.removeFirst();
             }
-            while (!dq.isEmpty() && dq.peekLast().num > a[i]) {
-                dq.removeLast();
-            }
-            dq.addLast(new N(i, a[i]));
-            d[i] = dq.peekFirst().num;
+            ans[r] = dq.peekFirst().val;
             l++;
             r++;
         }
-
         for (int i = 0; i < n; i++) {
-            bw.write(d[i] + " ");
+            bw.write(ans[i] + " ");
         }
         bw.close();
         br.close();
@@ -48,11 +53,11 @@ class Main {
 
     static class N {
         int idx;
-        int num;
+        int val;
 
-        N(int i, int n) {
+        N(int i, int v) {
             idx = i;
-            num = n;
+            val = v;
         }
     }
 }
