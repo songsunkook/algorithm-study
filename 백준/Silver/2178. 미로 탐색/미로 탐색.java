36bbe0a73023a828cfd99c9001a17634dd31be7d
송선권 <songsunkook@gmail.com>
2026-03-10 11:34:26 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14724 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,75 +1,61 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int N, M;
-    static boolean[][] visited, miro;
-    static List<List<Integer>> adj = new ArrayList<>();
+class Main {
+    static int R, C;
+    static boolean[][] arr, vis;
+    static int[][] dd = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        visited = new boolean[N][M];
-        miro = new boolean[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            String input = st.nextToken();
-            for (int j = 0; j < M; j++)
-                miro[i][j] = input.charAt(j) == '1';
-        }
-
-        //문제 풀이
-        Queue<Pair> queue = new LinkedList<>();
-        int count = 0;
-        boolean success = false;
-        queue.offer(new Pair(0, 0));
-        visited[0][0] = true;
-        while (!queue.isEmpty()) {
-            if (success)
-                break;
-            count++;
-            int qSize = queue.size();
-            for (int i = 0; i < qSize; i++) {
-                Pair curr = queue.poll();
-                if (curr.r == N - 1 && curr.c == M - 1) {
-                    success = true;
-                    break;
-                }
-                if (curr.r - 1 >= 0 && !visited[curr.r - 1][curr.c] && miro[curr.r - 1][curr.c]) {
-                    visited[curr.r - 1][curr.c] = true;
-                    queue.offer(new Pair(curr.r - 1, curr.c));
-                }
-                if (curr.r + 1 < N && !visited[curr.r + 1][curr.c] && miro[curr.r + 1][curr.c]) {
-                    visited[curr.r + 1][curr.c] = true;
-                    queue.offer(new Pair(curr.r + 1, curr.c));
-                }
-                if (curr.c - 1 >= 0 && !visited[curr.r][curr.c - 1] && miro[curr.r][curr.c - 1]) {
-                    visited[curr.r][curr.c - 1] = true;
-                    queue.offer(new Pair(curr.r, curr.c - 1));
-                }
-                if (curr.c + 1 < M && !visited[curr.r][curr.c + 1] && miro[curr.r][curr.c + 1]) {
-                    visited[curr.r][curr.c + 1] = true;
-                    queue.offer(new Pair(curr.r, curr.c + 1));
-                }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        arr = new boolean[R][C];
+        vis = new boolean[R][C];
+        for (int r = 0; r < R; r++) {
+            String line = br.readLine();
+            int i = 0;
+            for (char a : line.toCharArray()) {
+                arr[r][i] = a == '1';
+                i++;
             }
         }
-        bw.write(Integer.toString(count));
 
-        bw.flush();
+        Queue<N> q = new LinkedList<>();
+        q.offer(new N(0, 0, 1));
+        vis[0][0] = true;
+        while (!q.isEmpty()) {
+            N cur = q.poll();
+            if (cur.r == R - 1 && cur.c == C - 1) {
+                bw.write(cur.d + "");
+                break;
+            }
+            for (int[] d : dd) {
+                int nr = cur.r + d[0];
+                int nc = cur.c + d[1];
+                if (nr >= R || nr < 0 || nc >= C || nc < 0 || !arr[nr][nc] || vis[nr][nc]) {
+                    continue;
+                }
+                vis[nr][nc] = true;
+                q.offer(new N(nr, nc, cur.d + 1));
+            }
+        }
         bw.close();
         br.close();
     }
-}
 
-class Pair {
-    public int r, c;
+    static class N {
+        int r;
+        int c;
+        int d;
 
-    Pair(int r, int c) {
-        this.r = r;
-        this.c = c;
+        N(int rr, int cc, int dd) {
+            r = rr;
+            c = cc;
+            d = dd;
+        }
     }
 }
