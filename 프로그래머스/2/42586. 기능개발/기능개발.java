ff62defb02427ab08887a8167afeeed977c9cd3d
송선권 <songsunkook@gmail.com>
2026-03-11 10:11:46 +0900

    [level 2] Title: 기능개발, Time: 2.48 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,24 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        if (progresses.length == 0) return new int[0];
-        List<Integer> ans = new ArrayList<>();
+        // 뒷 작업부터 걸리는 시간을 stack에 삽입, 앞작업부터 pop하며 이미 걸린시간이 더 크다면 함께 pop
+        Stack<Integer> st = new Stack<>();
+        int n = speeds.length;
+        for(int i=n-1;i>=0;i--) {
+            int left = 100 - progresses[i];
+            int leftDays = (int)Math.ceil((double)left / speeds[i]);
+            st.push(leftDays);
+        }
         int today = 0;
-        int cnt = 1;
-        for(int i = 0;i<progresses.length;i++) {
-        	int left = 100 - progresses[i];
-            int days = (int)Math.ceil((double)left / speeds[i]);
-            if (days > today) {
-                today = days;
-                if (i != 0) {
-                	ans.add(cnt);
-                	cnt = 1;
-                }
-            } else {
+        int cnt = 0;
+        List<Integer> ans = new ArrayList<>();
+		while(!st.isEmpty()) {
+            while (!st.isEmpty() && today >= st.peek()) {
+            	st.pop();
+                cnt++;
+            }
+            if (cnt != 0) {
+            	ans.add(cnt);
+            	cnt = 0;
+            }
+            if (!st.isEmpty() && today < st.peek()) {
+                today = st.pop();
                 cnt++;
             }
         }
-        if (cnt != 0) ans.add(cnt);
+        if (cnt != 0) {
+            ans.add(cnt);
+        }
         return ans.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
