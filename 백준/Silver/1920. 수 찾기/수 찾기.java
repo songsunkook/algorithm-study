f7f0c570e7041ab4289f8c6b2af0fe92ee915cf5
송선권 <songsunkook@gmail.com>
2026-03-12 12:12:30 +0900

    [Silver IV] Title: 수 찾기, Time: 612 ms, Memory: 42556 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,36 +5,31 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            bw.write(solve(arr, target) ? "1\n" : "0\n");
+        for (int t = 0; t < m; t++) {
+            int ta = Integer.parseInt(st.nextToken());
+
+            int l = 0, r = n - 1;
+            while (l < r) {
+                int mid = (l + r) / 2;
+                if (arr[mid] < ta) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            }
+            bw.write(arr[l] == ta ? "1\n" : "0\n");
         }
+
         bw.close();
         br.close();
-    }
-
-    private static boolean solve(int[] arr, int target) {
-        int l = 0;
-        int r = arr.length - 1;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (target < arr[mid])
-                r = mid - 1;
-            else if (arr[mid] < target)
-                l = mid + 1;
-            else
-                return true;
-        }
-        return false;
     }
 }
