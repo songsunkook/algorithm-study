a5ba7e7bef0be623f22a5b9079235da0d431c3dd
송선권 <songsunkook@gmail.com>
2026-03-24 09:21:50 +0900

    [Gold IV] Title: N-Queen, Time: 3280 ms, Memory: 15136 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,53 +1,48 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 가로, 세로, 대각선을 유일하게 식별해야 한다.
- * 가로: 한줄씩 내려가며 진행
- * 세로: boolean[col]
- * / 대각선: boolean[i + j]
- * \ 대각선: boolean[i - j + n - 1] (음수 인덱스 방지)
- */
 class Main {
+
     static int n;
-    static boolean[] dia1, dia2, col;
+    static boolean[] co, d1, d2;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = Integer.parseInt(br.readLine());
-        dia1 = new boolean[2 * n];
-        dia2 = new boolean[2 * n];
-        col = new boolean[n];
+        co = new boolean[n];
+        d1 = new boolean[2 * n];
+        d2 = new boolean[2 * n];
+
         bw.write(dfs(0) + "");
         bw.close();
         br.close();
     }
 
-    static int dfs(int row) {
-        if (row == n) {
+    static int dfs(int r) {
+        if (r == n) {
             return 1;
         }
         int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (can(row, i)) {
-                put(row, i, true);
-                cnt += dfs(row + 1);
-                put(row, i, false);
+        for (int c = 0; c < n; c++) {
+            if (can(r, c)) {
+                put(r, c, true);
+                cnt += dfs(r + 1);
+                put(r, c, false);
             }
         }
         return cnt;
     }
 
     static boolean can(int r, int c) {
-        return r >= 0 && r < n && c >= 0 && c < n
-                && !col[c] && !dia1[r + c] && !dia2[r - c + n - 1];
+        return r >= 0 && r < n && c >= 0 && c < n &&
+                !co[c] && !d1[r + c] && !d2[r - c + n - 1];
     }
 
     static void put(int r, int c, boolean flag) {
-        col[c] = flag;
-        dia1[r + c] = flag;
-        dia2[r - c + n - 1] = flag;
+        co[c] = flag;
+        d1[r + c] = flag;
+        d2[r - c + n - 1] = flag;
     }
 }
