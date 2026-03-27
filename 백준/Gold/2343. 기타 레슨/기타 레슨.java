fb2effce5327db445dcfec67c5575366687650bc
송선권 <songsunkook@gmail.com>
2026-03-27 13:26:33 +0900

    [Gold V] Title: 기타 레슨, Time: 300 ms, Memory: 23756 KB -BaekjoonHub

--- 백준/Gold/2343. 기타 레슨/기타 레슨.java
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.java
@@ -1,54 +1,60 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 동일한 길이의 블루레이 m개에 모든 강의를 담는다.
- * 블루레이 길이를 최소화해야 한다.
- * 블루레이 m개에 모든 강의를 담을 수 있는 가장 작은 블루레이 길이는?
- * 
- * 임의의 큰 블루레이 길이를 정하고, 이걸 r로 두고 이분탐색한다.
- * 각 이분탐색 루프에서는 이 길이로 나눌 때 총 몇개의 블루레이가 나오는지 확인한다.
- * m개보다 많이 나오면 길이를 키우고(l = mid)
- * m개보다 적게 나오면 길이를 줄인다(r = mid)
- */
 class Main {
+
+    static int n, m;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        int max = 0, total = 0;
+        int max = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             max = Math.max(max, arr[i]);
-            total += arr[i];
         }
-
-        int l = max, r = total;
+        int l = max, r = 2000000000;
         while (l < r) {
             int mid = (l + r) / 2;
-
-            int sum = 0, cnt = 1;
-            for (int i = 0; i < n; i++) {
-                if (sum + arr[i] > mid) {
-                    cnt++;
-                    sum = 0;
-                }
-                sum += arr[i];
-            }
-
-            if (cnt <= m) {
-                r = mid;
-            } else {
+            int cnt = func(mid);
+            if (cnt >= m) {
                 l = mid + 1;
+            } else {
+                r = mid;
             }
+            // System.out.printf("(%d, %d): %d\n", l, r, cnt);
         }
         bw.write(l + "");
         bw.close();
         br.close();
+    }
+
+    static int func(int len) {
+        int sum = 0;
+        int cnt = -1;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            if (sum == len) {
+                sum = 0;
+                cnt++;
+            }
+            if (sum > len) {
+                sum = arr[i];
+                cnt++;
+            }
+            // System.out.printf("sum: %d, cnt: %d, len: %d\n", sum, cnt, len);
+        }
+        if (sum > 0) {
+            cnt++;
+        }
+        // System.out.println("cnt: " + cnt);
+        return cnt;
     }
 }
