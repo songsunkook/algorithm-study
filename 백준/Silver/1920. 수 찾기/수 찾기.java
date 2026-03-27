2381f1194e8090330f23d6eccd95c46a6a6ffa3e
송선권 <songsunkook@gmail.com>
2026-03-27 11:33:48 +0900

    [Silver IV] Title: 수 찾기, Time: 608 ms, Memory: 44160 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,28 +5,32 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int t = 0; t < m; t++) {
-            int ta = Integer.parseInt(st.nextToken());
-
+        for (int i = 0; i < m; i++) {
+            int t = Integer.parseInt(st.nextToken());
             int l = 0, r = n - 1;
             while (l < r) {
                 int mid = (l + r) / 2;
-                if (arr[mid] < ta) {
+                if (arr[mid] == t) {
+                    l = mid;
+                    break;
+                }
+                if (arr[mid] < t) {
                     l = mid + 1;
                 } else {
                     r = mid;
                 }
             }
-            bw.write(arr[l] == ta ? "1\n" : "0\n");
+            bw.write(arr[l] == t ? "1\n" : "0\n");
         }
 
         bw.close();
