1d59c998344deb23c965902dc87392008adae5c0
송선권 <songsunkook@gmail.com>
2026-03-30 15:15:18 +0900

    [Gold IV] Title: 수 묶기, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -7,7 +7,7 @@ import java.util.*;
  * 정렬 이후, 양쪽 끝에서 둘씩 묶어서 더하면 마지막에는 0(중앙)에서 모일 것.
  * 엣지 케이스
  * - 0은 가장 작은 절댓값의 음수와 묶을 수 있지만 양수와 묶으면 안된다.
- * - 1 * 1은 묶지 않아야 한다.
+ * - 1은 묶지 않아야 한다.
  */
 class Main {
     public static void main(String[] args) throws IOException {
@@ -15,42 +15,30 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        if (n == 1) {
-            bw.write(arr[0] + "");
-            bw.close();
-            br.close();
-            return;
-        }
-        Arrays.sort(arr);
-        int i = 0;
+        PriorityQueue<Integer> plus = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        PriorityQueue<Integer> minus = new PriorityQueue<>();
         long sum = 0;
-        while (i + 1 < n && arr[i + 1] <= 0) {
-            sum += arr[i] * arr[i + 1];
-            i += 2;
-        }
-        if (i + 1 < n && arr[i + 1] == 0) {
-            i += 2;
-        }
-        if (i < n && arr[i] < 0) {
-            sum += arr[i];
-        }
-        int mid = i;
-        i = n - 1;
-        while (i - 1 >= mid && arr[i - 1] > 0) {
-            if (arr[i] == 1 || arr[i - 1] == 1) {
-                sum += arr[i] + arr[i - 1];
-                i -= 2;
-                continue;
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(br.readLine());
+            if (a > 1) {
+                plus.offer(a);
+            } else if (a == 1) {
+                sum += 1;
+            } else {
+                minus.offer(a);
             }
-            sum += arr[i] * arr[i - 1];
-            i -= 2;
         }
-        if (i >= 0 && arr[i] > 0) {
-            sum += arr[i];
+        while (plus.size() > 1) {
+            sum += plus.poll() * plus.poll();
+        }
+        if (!plus.isEmpty()) {
+            sum += plus.poll();
+        }
+        while (minus.size() > 1) {
+            sum += minus.poll() * minus.poll();
+        }
+        if (!minus.isEmpty()) {
+            sum += minus.poll();
         }
         bw.write(sum + "");
         bw.close();
