50aa4a7f66321c0781ee75523fab5d995e21b57a
송선권 <songsunkook@gmail.com>
2026-04-04 15:11:42 +0900

    [Gold I] Title: K번째 수, Time: 132 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,25 +1,33 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 1 2 3 4 5
+ * 1 1 2 3 4 5
+ * 2 2 4 6 8 10
+ * 3 3 6 9 12 15
+ * 4 4 8 12 16 20
+ * 5 5 10 15 20 25
+ */
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
 
-        int l = 1, r = k;
+        int l = 0, r = a;
         while (l < r) {
             int mid = (l + r) / 2;
-            int cnt = 0;
-            for (int d = 1; d <= n; d++) {
-                cnt += Math.min(mid / d, n);
+            int sum = 0;
+            for (int dan = 1; dan <= n; dan++) {
+                sum += Math.min(mid / dan, n);
             }
-            if (cnt >= k) {
-                r = mid;
-            } else {
+            if (sum < a) {
                 l = mid + 1;
+            } else {
+                r = mid;
             }
         }
         bw.write(l + "");
