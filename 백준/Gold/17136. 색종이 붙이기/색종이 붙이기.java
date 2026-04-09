09c3d2322792a1b966e6a126b2912db4cbb81e0d
송선권 <songsunkook@gmail.com>
2026-04-09 10:38:33 +0900

    [Gold II] Title: 색종이 붙이기, Time: 272 ms, Memory: 23064 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -3,15 +3,14 @@ import java.util.*;
 
 class Main {
 
-    static int[] paper = new int[6];
     static boolean[][] arr;
+    static int[] paper = { 0, 5, 5, 5, 5, 5 };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         arr = new boolean[10][10];
-        Arrays.fill(paper, 5);
         for (int r = 0; r < 10; r++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int c = 0; c < 10; c++) {
@@ -19,49 +18,40 @@ class Main {
             }
         }
 
-        int cnt = dfs();
-        bw.write(cnt == Integer.MAX_VALUE ? "-1" : (cnt + ""));
+        int result = find(0);
+
+        bw.write(result == 2000000000 ? "-1" : (result + ""));
         bw.close();
         br.close();
     }
 
-    static int dfs() {
-        int min = Integer.MAX_VALUE;
-        boolean clear = true;
+    static int find(int cnt) {
         for (int r = 0; r < 10; r++) {
             for (int c = 0; c < 10; c++) {
                 if (arr[r][c]) {
-                    clear = false;
+                    int min = 2000000000;
                     for (int s = 5; s >= 1; s--) {
-                        if (can(r, c, s)) {
-                            put(r, c, s, false);
-                            int res = dfs();
-                            if (res != Integer.MAX_VALUE) {
-                                min = Math.min(min, res + 1);
-                            }
+                        if (canPut(r, c, s)) {
                             put(r, c, s, true);
+                            min = Math.min(min, find(cnt) + 1);
+                            put(r, c, s, false);
                         }
                     }
                     return min;
                 }
             }
         }
-        if (clear) {
-            return 0;
-        }
-        return min;
+        return 0;
     }
 
-    static boolean can(int r, int c, int s) {
-        if (paper[s] <= 0) {
+    static boolean canPut(int r, int c, int s) {
+        if (!(r >= 0 && r + s <= 10 && c >= 0 && c + s <= 10 && paper[s] > 0)) {
             return false;
         }
-        if (r < 0 || r + s > 10 || c < 0 || c + s > 10) {
-            return false;
-        }
-        for (int nr = r; nr < r + s; nr++) {
-            for (int nc = c; nc < c + s; nc++) {
-                if (!arr[nr][nc]) {
+
+        for (int ri = r; ri < r + s; ri++) {
+            for (int ci = c; ci < c + s; ci++) {
+                if (!arr[ri][ci]) {
                     return false;
                 }
             }
@@ -69,15 +59,16 @@ class Main {
         return true;
     }
 
-    static void put(int r, int c, int s, boolean flag) {
+    static void put(int sr, int sc, int s, boolean flag) {
         if (flag) {
-            paper[s]++;
-        } else {
             paper[s]--;
+        } else {
+            paper[s]++;
         }
-        for (int nr = r; nr < r + s; nr++) {
-            for (int nc = c; nc < c + s; nc++) {
-                arr[nr][nc] = flag;
+
+        for (int r = sr; r < sr + s; r++) {
+            for (int c = sc; c < sc + s; c++) {
+                arr[r][c] = !flag;
             }
         }
     }
