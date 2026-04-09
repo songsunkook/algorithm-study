16b1abae6288e791389a504b83d56c339ba35c97
송선권 <songsunkook@gmail.com>
2026-04-09 11:44:39 +0900

    [Gold II] Title: 트리의 지름, Time: 672 ms, Memory: 86524 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -3,58 +3,54 @@ import java.util.*;
 
 class Main {
 
-    static int v, dia, dep, depN;
-    static List<N>[] arr;
+    static int n, maxNode = 0, maxLen = 0;
     static boolean[] vis;
+    static List<N>[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        v = Integer.parseInt(br.readLine());
-        arr = new List[v];
-        vis = new boolean[v];
-        for (int i = 0; i < v; i++) {
+        n = Integer.parseInt(br.readLine());
+        arr = new ArrayList[n + 1];
+        vis = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
             arr[i] = new ArrayList<>();
         }
-        for (int i = 0; i < v; i++) {
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()) - 1;
+            int a = Integer.parseInt(st.nextToken());
             while (true) {
-                int b = Integer.parseInt(st.nextToken()) - 1;
-                if (b == -2) {
+                int b = Integer.parseInt(st.nextToken());
+                if (b == -1) {
                     break;
                 }
                 int c = Integer.parseInt(st.nextToken());
                 arr[a].add(new N(b, c));
-                arr[b].add(new N(a, c));
             }
         }
 
-        vis[0] = true;
-        dep = -1;
-        depN = -1;
-        dfs(0, 0);
+        vis[1] = true;
+        dfs(1, 0);
         Arrays.fill(vis, false);
-        vis[depN] = true;
-        dep = -1;
-        int aa = depN;
-        depN = -1;
-        dfs(aa, 0);
-        bw.write(dep + "");
+        vis[maxNode] = true;
+        dfs(maxNode, 0);
+
+        bw.write(maxLen + "");
         bw.close();
         br.close();
     }
 
-    static void dfs(int now, int nowD) {
-        if (dep < nowD) {
-            dep = nowD;
-            depN = now;
+    static void dfs(int node, int dep) {
+        if (maxLen < dep) {
+            maxNode = node;
+            maxLen = dep;
         }
-        for (N next : arr[now]) {
-            if (!vis[next.n]) {
-                vis[next.n] = true;
-                dfs(next.n, nowD + next.d);
+
+        for (N adj : arr[node]) {
+            if (!vis[adj.n]) {
+                vis[adj.n] = true;
+                dfs(adj.n, dep + adj.d);
             }
         }
     }
