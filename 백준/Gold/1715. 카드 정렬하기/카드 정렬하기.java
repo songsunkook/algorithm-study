dfc3d775413abfae103c351ddee00f63a9ed19c5
송선권 <songsunkook@gmail.com>
2026-04-10 14:16:55 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 360 ms, Memory: 25752 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -6,16 +6,18 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
             pq.offer(Integer.parseInt(br.readLine()));
         }
+
         long sum = 0;
         while (pq.size() > 1) {
-            int com = pq.poll() + pq.poll();
-            sum += com;
-            pq.offer(com);
+            int a = pq.poll();
+            int b = pq.poll();
+            pq.offer(a + b);
+            sum += a + b;
         }
         bw.write(sum + "");
         bw.close();
