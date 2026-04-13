f71991255c5ef0b667eea90631760ef6594d771e
송선권 <songsunkook@gmail.com>
2026-04-13 11:35:02 +0900

    [Gold II] Title: 색종이 붙이기, Time: 288 ms, Memory: 23060 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -3,55 +3,54 @@ import java.util.*;
 
 class Main {
 
-    static boolean[][] arr;
+    static boolean[][] arr = new boolean[10][10];
     static int[] paper = { 0, 5, 5, 5, 5, 5 };
+    static int min = 100;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        arr = new boolean[10][10];
         for (int r = 0; r < 10; r++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int c = 0; c < 10; c++) {
                 arr[r][c] = Integer.parseInt(st.nextToken()) == 1;
             }
         }
-
-        int result = find(0);
-
-        bw.write(result == 2000000000 ? "-1" : (result + ""));
+        dfs(0);
+        if (min == 100) {
+            min = -1;
+        }
+        bw.write(min + "");
         bw.close();
         br.close();
     }
 
-    static int find(int cnt) {
+    static void dfs(int cnt) {
         for (int r = 0; r < 10; r++) {
             for (int c = 0; c < 10; c++) {
                 if (arr[r][c]) {
-                    int min = 2000000000;
                     for (int s = 5; s >= 1; s--) {
-                        if (canPut(r, c, s)) {
+                        if (can(r, c, s)) {
                             put(r, c, s, true);
-                            min = Math.min(min, find(cnt) + 1);
+                            dfs(cnt + 1);
                             put(r, c, s, false);
                         }
                     }
-                    return min;
+                    return;
                 }
             }
         }
-        return 0;
+        min = Math.min(min, cnt);
     }
 
-    static boolean canPut(int r, int c, int s) {
-        if (!(r >= 0 && r + s <= 10 && c >= 0 && c + s <= 10 && paper[s] > 0)) {
+    static boolean can(int rr, int cc, int s) {
+        if (!(paper[s] > 0 && rr >= 0 && cc >= 0 && rr + s <= 10 && cc + s <= 10)) {
             return false;
         }
-
-        for (int ri = r; ri < r + s; ri++) {
-            for (int ci = c; ci < c + s; ci++) {
-                if (!arr[ri][ci]) {
+        for (int r = rr; r < rr + s; r++) {
+            for (int c = cc; c < cc + s; c++) {
+                if (!arr[r][c]) {
                     return false;
                 }
             }
@@ -59,15 +58,10 @@ class Main {
         return true;
     }
 
-    static void put(int sr, int sc, int s, boolean flag) {
-        if (flag) {
-            paper[s]--;
-        } else {
-            paper[s]++;
-        }
-
-        for (int r = sr; r < sr + s; r++) {
-            for (int c = sc; c < sc + s; c++) {
+    static void put(int rr, int cc, int s, boolean flag) {
+        paper[s] += flag ? -1 : 1;
+        for (int r = rr; r < rr + s; r++) {
+            for (int c = cc; c < cc + s; c++) {
                 arr[r][c] = !flag;
             }
         }
